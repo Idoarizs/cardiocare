bb87c5189967e87c7425a430836f0c5a7b4d45c0
oline jkt48 <olinejkt48@gmail.com>
2024-12-22 16:22:46 +0700

    fixing cors error

--- backend/app.py
+++ backend/app.py
@@ -1,9 +1,13 @@
 from flask import Flask
 from flask_restful import Api
+from flask_cors import CORS
+
 from routes import routes 
 
 app = Flask(__name__)
-api = Api(app)
+api = Api(app)      
+
+CORS(app)
 
 # Routes
 api.add_resource(routes.ModelPredict, '/predict')
